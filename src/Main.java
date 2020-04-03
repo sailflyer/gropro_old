c8423f91d6c12bd2fdc89d7e62281c9ed29085e2
sailflyer <32387636+sailflyer@users.noreply.github.com>
2020-04-03 18:22:08 +0200

    created Main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main{
+  
+  public static void main(String[] args){
+    if(args.length > 2){
+      for(int i = 2; i < args.length){
+        // Aufruf des Controller Interface mit der Übergabe der Eingabedatei in args[i]
+      }
+    }
+  }
+  
+}
